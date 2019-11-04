b46f284eb7e21bd981edec959885d1216e6640ba
K4USTU3H <k4ustu3h@gmail.com>
2019-11-04 17:48:02 +0530

    Removed Travis CI and unnecessary scripts

--- dist/js/darkmode.js
+++ dist/js/darkmode.js
@@ -4,38 +4,46 @@
 
 const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'light');
-    }
-    else {
-        document.documentElement.setAttribute('data-theme', 'dark');
-    }
+function switchTheme(e)
+{
+  if (e.target.checked)
+  {
+    document.documentElement.setAttribute('data-theme', 'light');
+  }
+  else
+  {
+    document.documentElement.setAttribute('data-theme', 'dark');
+  }
 }
 
 toggleSwitch.addEventListener('change', switchTheme, false);
 
 // Store the user preference for future visits
 
-function switchTheme(e) {
-    if (e.target.checked) {
-        document.documentElement.setAttribute('data-theme', 'light');
-        localStorage.setItem('theme', 'light'); //add this
-    }
-    else {
-        document.documentElement.setAttribute('data-theme', 'dark');
-        localStorage.setItem('theme', 'dark'); //add this
-    }
+function switchTheme(e)
+{
+  if (e.target.checked)
+  {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light'); //add this
+  }
+  else
+  {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark'); //add this
+  }
 }
 
 // Check for saved user preference, if any, on load of the website
 
 const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : null;
 
-if (currentTheme) {
-    document.documentElement.setAttribute('data-theme', currentTheme);
+if (currentTheme)
+{
+  document.documentElement.setAttribute('data-theme', currentTheme);
 
-    if (currentTheme === 'light') {
-        toggleSwitch.checked = true;
-    }
+  if (currentTheme === 'light')
+  {
+    toggleSwitch.checked = true;
+  }
 }
