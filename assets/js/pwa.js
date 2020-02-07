e5bb9fc58f45cc8064c5f6b68b60de0ec0fcc50e
Kaustubh Ladiya (via Travis CI) <k4ustu3h@gmail.com>
2020-02-07 18:03:40 +0000

    Deploy kotin.ga to github.com/k4ustu3h/kotin.ga.git:gh-pages

--- assets/js/pwa.js
+++ assets/js/pwa.js
@@ -1,54 +1,41 @@
-"use strict";
-var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault"),
-  _regenerator = _interopRequireDefault(require("@babel/runtime/regenerator")),
-  _asyncToGenerator2 = _interopRequireDefault(
-    require("@babel/runtime/helpers/asyncToGenerator")
-  );
-"serviceWorker" in navigator &&
+if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker.register("/sw.js");
   });
-var pwaInstalled = "yes" == localStorage.getItem("pwaInstalled");
-window.matchMedia("(display-mode: standalone)").matches &&
-  (localStorage.setItem("pwaInstalled", "yes"), (pwaInstalled = !0)),
-  !0 === window.navigator.standalone &&
-    (localStorage.setItem("pwaInstalled", "yes"), (pwaInstalled = !0)),
-  (document.getElementById("installPWA").style.display = pwaInstalled
-    ? "none"
-    : "block");
-var deferredPrompt = null;
-function installPWA() {
-  return _installPWA.apply(this, arguments);
 }
-function _installPWA() {
-  return (_installPWA = (0, _asyncToGenerator2.default)(
-    _regenerator.default.mark(function e() {
-      return _regenerator.default.wrap(function e(t) {
-        for (;;)
-          switch ((t.prev = t.next)) {
-            case 0:
-              deferredPrompt &&
-                (deferredPrompt.prompt(),
-                deferredPrompt.userChoice.then(function(e) {
-                  var t;
-                  "accepted" === e.outcome
-                    ? console.log("Your PWA has been installed")
-                    : console.log("User chose to not install your PWA"),
-                    (deferredPrompt = null);
-                }));
-            case 1:
-            case "end":
-              return t.stop();
-          }
-      }, e);
-    })
-  )).apply(this, arguments);
+let pwaInstalled = localStorage.getItem("pwaInstalled") == "yes";
+if (window.matchMedia("(display-mode: standalone)").matches) {
+  localStorage.setItem("pwaInstalled", "yes");
+  pwaInstalled = true;
 }
-window.addEventListener("beforeinstallprompt", function(e) {
+if (window.navigator.standalone === true) {
+  localStorage.setItem("pwaInstalled", "yes");
+  pwaInstalled = true;
+}
+if (pwaInstalled) {
+  document.getElementById("installPWA").style.display = "none";
+} else {
+  document.getElementById("installPWA").style.display = "block";
+}
+let deferredPrompt = null;
+window.addEventListener("beforeinstallprompt", e => {
   deferredPrompt = e;
-}),
-  window.addEventListener("appinstalled", function(e) {
-    localStorage.setItem("pwaInstalled", "yes"),
-      (pwaInstalled = !0),
-      (document.getElementById("installPWA").style.display = "none");
-  });
+});
+async function installPWA() {
+  if (deferredPrompt) {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then(({ outcome }) => {
+      if (outcome === "accepted") {
+        console.log("Your PWA has been installed");
+      } else {
+        console.log("User chose to not install your PWA");
+      }
+      deferredPrompt = null;
+    });
+  }
+}
+window.addEventListener("appinstalled", evt => {
+  localStorage.setItem("pwaInstalled", "yes");
+  pwaInstalled = true;
+  document.getElementById("installPWA").style.display = "none";
+});
